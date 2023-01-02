c1c7cb2aec42eff7a5470bb13df64ae1b7f2ccb1
th-release <th.release.cth@gmail.com>
2023-01-02 14:08:10 +0900

    feat: Menu file path change

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,7 +1,7 @@
 import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
+import Menu from './components/Menu/Menu';
 import Page from './pages/Page';
 
 /* Core CSS required for Ionic components to work properly */
