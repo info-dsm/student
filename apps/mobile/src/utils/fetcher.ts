c1c58afa0a900c3f81ca8e68f7cb96cbea30f3ba
th-release <th.release.cth@gmail.com>
2023-01-02 14:59:24 +0900

    delete: fetcher.ts

--- apps/mobile/src/utils/fetcher.ts
+++
@@ -1,7 +0,0 @@
-import fetch from "unfetch";
-const fetcher = async (path: string, options: object) => {
-  const response = await fetch(path, options);
-  return response.json();
-};
-
-export default fetcher;
