abc1bc080ee8618a4ade7db84863b0b6ec1c00b3
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-02 22:55:15 +0900

    feat: add `loading page`, `token check`, `error`

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -36,50 +36,71 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import useSWR from 'swr'
+import Load from './components/Loading/LoadComponent';
+import useSWR from 'swr' 
+import authFetcher from './utils/AuthFetcher';
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/">
-            {/* 페이지 연결 */}
-          </Route>
-          <Route exact path="/">
-            {/* 페이지 연결 */}
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/panel">
-            <IonIcon ios={businessOutline} md={businessSharp} icon={business} />
-            <IonLabel>회사목록</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/report">
-            <IonIcon ios={globeOutline} md={globeSharp} icon={globe} />
-            <IonLabel>모집공고</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/admin">
-            <IonIcon ios={statsChartOutline} md={statsChartSharp} icon={statsChart} />
-            <IonLabel>취업현황</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab4" href="/admin">
-            <IonIcon ios={personOutline} md={personSharp} icon={person} />
-            <IonLabel>내정보</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab5" href="/admin">
-            <IonIcon ios={cogOutline} md={cogSharp} icon={cog} />
-            <IonLabel>문의</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const { data, error } = useSWR("url", authFetcher)
+
+  if (error) {
+    return (
+      // error 페이지가 현재 준비되지 않아서 일단 이렇게 해놈
+      <IonApp>{error}</IonApp>
+    )
+  }
+
+  if (!data) {
+    return (
+      <IonApp>
+        <Load />
+      </IonApp>
+    )
+  } else {
+    return (
+      <IonApp>
+        <IonReactRouter>
+          <IonTabs>
+            <IonRouterOutlet>
+              <Route exact path="/account/login">
+                <Load />
+              </Route>
+              <Route exact path="/">
+                {/* 페이지 연결 */}
+              </Route>
+              <Route exact path="/">
+                <Redirect to="/account/login" />
+              </Route>
+            </IonRouterOutlet>
+            <IonTabBar slot="bottom">
+              <IonTabButton tab="tab1" href="/list">
+                <IonIcon ios={businessOutline} md={businessSharp} icon={business} />
+                <IonLabel>회사목록</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab2" href="/report">
+                <IonIcon ios={globeOutline} md={globeSharp} icon={globe} />
+                <IonLabel>모집공고</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab3" href="/admin">
+                <IonIcon ios={statsChartOutline} md={statsChartSharp} icon={statsChart} />
+                <IonLabel>취업현황</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab4" href="/admin">
+                <IonIcon ios={personOutline} md={personSharp} icon={person} />
+                <IonLabel>내정보</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab5" href="/admin">
+                <IonIcon ios={cogOutline} md={cogSharp} icon={cog} />
+                <IonLabel>문의</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+      </IonApp>
+    );
+  }
+}
 
 export default App;
