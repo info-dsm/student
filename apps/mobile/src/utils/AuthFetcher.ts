a00725684db2b13a73514aa188e8cc657bdb0970
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-04 18:34:20 +0900

    feat: Update `AuthFetcher.ts`

--- apps/mobile/src/utils/AuthFetcher.ts
+++ apps/mobile/src/utils/AuthFetcher.ts
@@ -2,7 +2,7 @@ import fetch from "axios";
 import { getCookie } from "./cookies";
 
 const authFetcher = async (path: string, options: any) => {
-  const response = await fetch(path, {
+  const response = await fetch(process.env.REACT_APP_BASE + path, {
     method: "GET", 
     headers: {
       "Authorization": `Bearer ${getCookie("info-token")}`
