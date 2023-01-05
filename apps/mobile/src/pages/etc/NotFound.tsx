543162e0bc392595ece7c15dc05c2bf90fada883
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-05 09:18:29 +0900

    feat: Create `NotFound.tsx`

---
+++ apps/mobile/src/pages/etc/NotFound.tsx
@@ -0,0 +1,11 @@
+import { IonApp } from "@ionic/react";
+import etcStyle from "./etc.module.css"
+const NotFound: React.FC = () => {
+  return (
+    <IonApp>
+      
+    </IonApp>
+  )
+}
+
+export default NotFound;
