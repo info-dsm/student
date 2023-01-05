b2005870def0d20d78979504e66df3e5b230d3a1
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-05 09:38:36 +0900

    feat: Update `NotFound.tsx`

--- apps/mobile/src/pages/etc/NotFound.tsx
+++ apps/mobile/src/pages/etc/NotFound.tsx
@@ -2,8 +2,15 @@ import { IonApp } from "@ionic/react";
 import etcStyle from "./etc.module.css"
 const NotFound: React.FC = () => {
   return (
-    <IonApp>
+    <IonApp className={etcStyle.NotFound}>
+      <div>
+        <h1>404</h1>
+        <p>PAGE NOT FOUND</p>
+      </div>
       
+      <button className={etcStyle.Back}>
+        Go Back Home
+      </button>
     </IonApp>
   )
 }
