e5331b7258c97be6b7e663c23e40c6177f4632ed
jingeon27 <jingeon27@gmail.com>
2023-01-07 03:04:28 +0900

    Feat :: ToastComponents완성

--- apps/web/src/atom/index.ts
+++ apps/web/src/atom/index.ts
@@ -1,1 +1,1 @@
-import { atom } from "recoil";
+export {};
--- packages/ui/components/Toast.tsx
+++ packages/ui/components/Toast.tsx
@@ -7,20 +7,31 @@ export interface ToastProps {
 export const Toast = ({ label, children }: ToastProps) => {
   return (
     <>
-      <_Toast>{children}</_Toast>
+      <_Toast>
+        <_Layout>
+          <_Title>
+            {label === "company" ? "회사 리스트" : "모집공고 리스트"}
+          </_Title>
+          {children}
+        </_Layout>
+      </_Toast>
     </>
   );
 };
 const _Toast = styled.div`
   width: 70rem;
-  height: 35rem;
+  height: 37rem;
   background-color: ${(props) => props.theme.colors.violet};
   padding-top: 1.5rem;
 `;
 const _Layout = styled.div`
-  height: ;
+  height: 32rem;
+  width: 66rem;
+  justify-content: space-between;
+  margin: 0 auto;
 `;
 const _Title = styled.h1`
+  margin-left: 1rem;
   color: ${(props) => props.theme.colors.blue};
   font: 700 normal 1.2rem "pretendard", sans-serif;
 `;
