fc83acf3a278151c332695b36c842da664ed7c14
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-10 09:42:41 +0900

    feat: Update `Company.tsx`

--- apps/mobile/src/pages/company/Company.tsx
+++ apps/mobile/src/pages/company/Company.tsx
@@ -1,6 +1,12 @@
+import { IonApp } from "@ionic/react";
+import Header from "../../components/Header";
+
 const Company: React.FC = () => {
   return (
-    <></>
+    <IonApp>
+      <Header />
+      <div></div>
+    </IonApp>
   )
 }
 
