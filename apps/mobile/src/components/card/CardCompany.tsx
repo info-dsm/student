abac18e9e74711795d9f136c8aa79b738679ed8e
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-11 21:03:40 +0900

    feat: Create `Card Component`

---
+++ apps/mobile/src/components/card/CardCompany.tsx
@@ -0,0 +1,11 @@
+import { cardProps } from "../../type/interfaces"
+
+const CardCompany: React.FC<cardProps> = ({  }) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CardCompany;
