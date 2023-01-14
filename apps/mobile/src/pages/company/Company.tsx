2a7d7161c4481ce697927066279520714cc9893a
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-14 20:11:40 +0900

    feat: Update `Company.tsx`

--- apps/mobile/src/pages/company/Company.tsx
+++ apps/mobile/src/pages/company/Company.tsx
@@ -1,12 +1,22 @@
-import { IonApp } from "@ionic/react";
+import MouBanner from "../../components/banners/MouBanner";
 import Header from "../../components/Header";
+import RecruitmentBanner from "../../components/banners/RecruitmentBanner";
+import SearchBar from '../../components/searchBar/SearchBar';
+import CardCompany from "../../components/card/CardCompany";
+
+const tag = [{ name: "선도기업" }, { name: "MOU 체결" }]
 
 const Company: React.FC = () => {
   return (
-    <IonApp>
+    <div>
       <Header />
-      <div></div>
-    </IonApp>
+      <div className="contentBody">
+        <MouBanner />
+        <RecruitmentBanner />
+        <SearchBar url="/company" placeholder="검색어를 입력해주세요. (예. 회사, 직군, 경력 등)" />
+        <CardCompany title="(주)순양물산" desc="운송업을 주 사업으로 활동하는 회사 입니다." image="" tag={tag} />
+      </div>
+    </div>
   )
 }
 
