bfa71a7e0840bd6b861e577f783c55fe34922c44
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-05 21:36:07 +0900

    feat: Create App.tsx

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,81 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonRouterOutlet,
+  setupIonicReact
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+import useSWR from 'swr'
+import { fetcher } from './utils/authFetcher';
+setupIonicReact();
+
+const App: React.FC = () => {
+  const { data, error } = useSWR("/user/info", fetcher)
+  
+  return (
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/account/login">
+            {/* 사용자가 login(로그인)을 시도하는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/account/signup">
+            {/* 사용자가 signup(회원가입)을 시도하는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/company">
+            {/* 회사들을 표시 해주는 회사목록페이지 입니다. */}
+          </Route>
+          <Route exact path="/company/:id">
+            {/* 회사의 정보를 자세히 볼 수 있는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/company/search/:id">
+            {/* 회사를 검색 후 검색 결과를 보여주는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/notice">
+            {/* 회사들의 모집공고를 표시해주는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/notice/search/:id">
+            {/* 모집공고를 검색 후 검색 결과를 보여주는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/notice/:id">
+            {/* 모집공고를 자세히 볼 수 있는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/mypage">
+            {/* 내 계정의 정보를 보여주는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/recruit/:id">
+            {/* 취업 현황을 보여주는 페이지 입니다. */}
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/main" />
+          </Route>
+          <Route exact path="*">
+            
+          </Route>
+        </ IonRouterOutlet>
+      </IonReactRouter>
+    </IonApp>
+  );
+}
+
+export default App;
