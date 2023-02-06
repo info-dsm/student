5e21814445fa0945be87cec6dae148ebcb297430
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-06 21:17:07 +0900

    feat: Create CardNotice.tsx

---
+++ apps/mobile/src/components/cards/CardNotice.tsx
@@ -0,0 +1,30 @@
+import { NoticeCardProps } from "../../utils/interfaces";
+import { Body, NoticeContent, Image, NotImage } from "./cards";
+
+export const CardNotice: React.FC<NoticeCardProps> = ({ title, tags, company, image, address }) => {
+  return (
+    <Body>
+      {!image ? 
+        <NotImage>
+          <Image src="/assets/default.png" />
+        </NotImage>
+      :
+        <Image src={image} />
+      }
+      <NoticeContent>
+        <h1>{title}</h1>
+        <div>
+          <p><b>기업</b> &nbsp; {company}</p>
+          <p><b>주소</b> &nbsp; {address}</p>
+        </div>
+        <ul>
+          {tags.map((tag: { id:string }) => (
+            <li>
+              {tag.id}
+            </li>
+          ))}
+        </ul>
+      </NoticeContent>
+    </Body>
+  ) 
+}
