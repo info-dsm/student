50fef932930252bfb54c3ee313b65c5b8ff69ab1
eastcopper <ldh7228@gmail.com>
2023-02-13 17:57:51 +0900

    Create StudentMain

---
+++ apps/web/src/pages/student/main/Arrow.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Arrow from "../../../../public/assets/arrow";
+
+const ArrowText = ({ text, link }: { text: string; link: string }) => {
+  return (
+    <ArrowDiv>
+      <a href={link}>{text}</a>
+      <Arrow color={"#fff"} />
+    </ArrowDiv>
+  );
+};
+
+export default ArrowText;
+
+const ArrowDiv = styled.span`
+  a {
+    text-decoration: none;
+    color: rgba(255, 255, 255, 0.7);
+  }
+  position: absolute;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 19.09px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
