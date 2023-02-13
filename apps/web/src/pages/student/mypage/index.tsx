a2971b9de28406a3c2996bc8ea2d3498f31092ae
eastcopper <ldh7228@gmail.com>
2023-02-13 17:58:50 +0900

    Create StudentMyPage

---
+++ apps/web/src/pages/student/mypage/index.tsx
@@ -0,0 +1,77 @@
+import {
+  getUserInfo,
+  getUserInfoProps,
+  getSupportStatus,
+  getSupportStatusProps,
+} from "apis";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import HeaderComponent from "ui/components/Header";
+import StudentMyPageProfile from "./Profile";
+import StudentSupportStatus from "./SupportStatus";
+
+const StudentMyPage = () => {
+  const [info, setInfo] = useState<getUserInfoProps>();
+  const [status, setStatus] = useState<getSupportStatusProps[]>();
+
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      sessionStorage.getItem("accessToken")
+    ) {
+      getUserInfo().then((res) => {
+        console.log(res)
+        setInfo(res as getUserInfoProps);
+      });
+      getSupportStatus()
+        .then((res) => {
+          console.log(res)
+          setStatus(res as getSupportStatusProps[]);
+        })
+        .catch((err) => {
+          setStatus([]);
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      <HeaderComponent />
+      <MainDiv>
+        {info && status ? (
+          <>
+            <div>
+              <StudentMyPageProfile info={info} />
+              <StudentSupportStatus status={status} />
+            </div>
+          </>
+        ) : (
+          <div>
+            <StudentMyPageProfile
+              info={{
+                name: "로그인을 해주세요",
+                email: "",
+              }}
+            />
+          </div>
+        )}
+      </MainDiv>
+    </>
+  );
+};
+
+export default StudentMyPage;
+
+const MainDiv = styled.div`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  background-color: #f8f8f9;
+  > div {
+    min-height: 1000px;
+    width: 1448px;
+    padding: 150px 100px;
+    background-color: #fff;
+    box-shadow: 2px 2px 12px -1px rgba(0, 0, 0, 0.22);
+  }
+`;
