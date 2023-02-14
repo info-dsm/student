9eb8715ee3e26b900a0e14419302a1194f621ec6
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-14 14:39:30 +0900

    fix: Update snippet.ts

--- apps/mobile/src/utils/snippet.ts
+++ apps/mobile/src/utils/snippet.ts
@@ -1,5 +1,5 @@
 export default function snippet(text: string, length: number) {
-  var str = "";
+  var str = text;
   if (text.length > length) {
     str = text.substr(0, length - 2) + '...';
   }
