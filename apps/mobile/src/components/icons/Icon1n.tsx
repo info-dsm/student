960788839fe058fed7506ea457c8c8e521f9a4d4
th.release.cth <84012697+th-release@users.noreply.github.com>
2023-02-14 14:16:50 +0900

    feat: Rename icons

--- apps/mobile/src/components/icons/Icon1n.tsx
+++ apps/mobile/src/components/icons/Icon1n.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components"
 
-export const Icon1x: React.FC = () => {
+export const Icon1n: React.FC = () => {
   return (
     <>
       <InfoIcon xmlns="http://www.w3.org/2000/svg" width="39" height="37" viewBox="0 0 39 37" fill="none">
-        <path d="M0 2.64286H17.376C26.9725 2.64286 34.752 10.334 34.752 19.8214C34.752 29.3089 26.9725 37 17.376 37C7.77951 37 0 29.3089 0 19.8214V2.64286Z" fill="white" fill-opacity="0.7"/>
+        <path d="M0 2.64286H17.376C26.9725 2.64286 34.752 10.334 34.752 19.8214C34.752 29.3089 26.9725 37 17.376 37C7.77951 37 0 29.3089 0 19.8214V2.64286Z" fill="white" fillOpacity="0.7"/>
         <path d="M5.34647 0L5.34647 16.5179C5.34647 25.6404 12.8268 33.0357 22.0542 33.0357C31.2816 33.0357 38.7619 25.6404 38.7619 16.5179C38.7619 7.3953 31.2816 1.12078e-06 22.0542 7.22019e-07L5.34647 0Z" fill="white"/>
       </InfoIcon>
       <InfoText width="65" height="29" viewBox="0 0 65 29" fill="none" xmlns="http://www.w3.org/2000/svg">
