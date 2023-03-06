b6003b8837866f3380c47ae1cdd1812d50efaa3c
ldh7228 <ldh7228@gmail.com>
2023-03-06 23:45:01 +0900

    Add Carouse

--- apps/student/src/pages/company/detail/[...id].tsx
+++ apps/student/src/pages/company/detail/[...id].tsx
@@ -17,6 +17,7 @@ const StudentCompanyDetail = ({}: {}) => {
   const [info, setInfo] = useState<getCompanyDetailProps>();
   const [noticeInfo, setNoticeInfo] =
     useState<getWaitingNoticeListContentProps[]>();
+  const [current, setCurrent] = useState<number>(0);
 
   useEffect(() => {
     if (query) {
@@ -38,15 +39,43 @@ const StudentCompanyDetail = ({}: {}) => {
         <DetailDiv>
           {info && noticeInfo ? (
             <>
-              <img
-                src={
-                  info.companyIntroductionResponse.companyLogo.fileUrl
-                    ? info.companyIntroductionResponse.companyLogo.fileUrl
-                    : ""
-                }
-                alt="company logo"
-                placeholder="blur"
-              />
+              <Carousel>
+                <div
+                  onClick={() => {
+                    if (current === 0)
+                      setCurrent(
+                        info.companyIntroductionResponse.companyPhotoList
+                          .length - 1
+                      );
+                    else setCurrent(current - 1);
+                  }}
+                >
+                  {"<"}
+                </div>
+                {info.companyIntroductionResponse.companyPhotoList.map((t) => (
+                  <span>
+                    <CarouselImg
+                      translateX={current * -1000}
+                      src={t.fileUrl}
+                      alt="company photo"
+                      placeholder="blur"
+                    />
+                  </span>
+                ))}
+                <div
+                  onClick={() => {
+                    if (
+                      current <
+                      info.companyIntroductionResponse.companyPhotoList.length -
+                        1
+                    )
+                      setCurrent(current + 1);
+                    else setCurrent(0);
+                  }}
+                >
+                  {">"}
+                </div>
+              </Carousel>
               <h1>㈜ {info.companyName}</h1>
               <DetailInfo companyInfo={info} subData={""} />
               <StudentCompanyNoticeList companyInfo={info} info={noticeInfo} />
@@ -63,6 +92,43 @@ const StudentCompanyDetail = ({}: {}) => {
 
 export default StudentCompanyDetail;
 
+const Carousel = styled.div`
+  display: inline-flex;
+  white-space: nowrap;
+  width: 1000px;
+  height: 600px;
+  overflow-x: hidden;
+  position: relative;
+  text-align: center;
+  overflow-y: hidden;
+
+  span {
+    text-align: center;
+  }
+
+  div {
+    z-index: 2;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 50px;
+    font-weight: 900;
+    left: 30px;
+    cursor: pointer;
+    ~ div {
+      left: 940px;
+    }
+  }
+`;
+
+const CarouselImg = styled.img<{ translateX: number }>`
+  width: 1000px;
+  height: 600px;
+  object-fit: contain;
+  transition: 1s;
+  transform: translateX(${(props) => props.translateX}px);
+`;
+
 const MainDiv = styled.div`
   display: flex;
   justify-content: center;
@@ -76,12 +142,6 @@ const DetailDiv = styled.div`
   height: 100%;
   background-color: #fff;
   padding: 174px 100px;
-
-  > img {
-    width: 1000px;
-    height: 600px;
-    object-fit: contain;
-  }
 
   > h1 {
     margin: 0;
