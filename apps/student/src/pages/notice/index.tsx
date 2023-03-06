93a1756c53d7c189d7fe6938b5e5e55f92ee9eb9
ldh7228 <ldh7228@gmail.com>
2023-03-06 13:25:08 +0900

    Error Handling

--- apps/student/src/pages/notice/index.tsx
+++ apps/student/src/pages/notice/index.tsx
@@ -61,38 +61,44 @@ const StudentNoticeList = () => {
       <HeaderComponent />
       <StudentNoticeBanner />
       <MainDiv>
-        <Kind>
+        {/* <Kind>
           <div>
             <div>모집공고</div>
             <span>마감 일자순 정렬</span>
           </div>
           <div>
             <div>마감된 공고</div>
-            {/* <div>
-              <Arrow color={"#4d4d4d"} />
-              <Arrow color={"#4d4d4d"} />
-            </div> */}
           </div>
-        </Kind>
+        </Kind> */}
 
         <Content>
-          {notice.length > 0 ? (
-            <>
-              <NoticeContainer id="noticeContainer">
-                {notice.map((t) => (
-                  // eslint-disable-next-line react/jsx-key
-                  <StudentNotice info={t} />
-                ))}
-              </NoticeContainer>
-            </>
-          ) : (
-            <>
-              <NoticePlaceHolder />
-              <NoticeContainer id="noticeContainer" style={{ width: "0px" }} />
-            </>
-          )}
+          <Kind2>
+            <div>
+              <span>모집공고</span>
+              <span>마감 일자순 정렬</span>
+            </div>
+            {notice.length > 0 ? (
+              <>
+                <NoticeContainer id="noticeContainer">
+                  {notice.map((t) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <StudentNotice info={t} />
+                  ))}
+                </NoticeContainer>
+              </>
+            ) : (
+              <>
+                <NoticePlaceHolder />
+                <NoticeContainer
+                  id="noticeContainer"
+                  style={{ width: "0px" }}
+                />
+              </>
+            )}
+          </Kind2>
 
-          <div>
+          <Kind2>
+            <div><span>마감된 공고</span></div>
             {closedNotice.length > 0 ? (
               <>
                 {closedNotice.map((t) => (
@@ -103,7 +109,7 @@ const StudentNoticeList = () => {
             ) : (
               <ClosedNoticePlaceHolder />
             )}
-          </div>
+          </Kind2>
         </Content>
       </MainDiv>
     </>
@@ -113,14 +119,36 @@ const StudentNoticeList = () => {
 export default StudentNoticeList;
 
 const MainDiv = styled.div`
-  width: 100%;
+  width: 100vw;
   height: 100%;
   background-color: #f8f8f9;
+  display: flex;
+  justify-content: center;
 `;
 
 const Content = styled.div`
   padding: 0px 236px;
   display: flex;
+`;
+
+const Kind2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 80px;
+  > div:nth-child(1) {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    margin-bottom: 30px;
+    span {
+      font-weight: 600;
+      font-size: 24px;
+      + span {
+        font-weight: 400;
+        font-size: 17px;
+      }
+    }
+  }
 `;
 
 const ClosedNoticeContainer = styled.div``;
