785bea801b21c80d6b4afff42f96d3048b7f12b1
ldh7228 <ldh7228@gmail.com>
2023-03-06 21:38:12 +0900

    add reissue

--- packages/ui/components/StudentHeader.tsx
+++ packages/ui/components/StudentHeader.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import TitleIcon from "../images/title";
 import cookie from "js-cookie";
 import { useRouter } from "next/router";
+import { reissue } from "../../../apps/student/src/axios/dist";
 
 const HeaderComponent = () => {
   const router = useRouter();
@@ -15,6 +16,13 @@ const HeaderComponent = () => {
     },
   ];
 
+  useEffect(() => {
+    reissue().catch(() => {
+      cookie.remove("accessToken");
+      cookie.remove("refreshToken");
+    });
+  }, []);
+
   return (
     <>
       <MainDiv>
@@ -23,7 +31,23 @@ const HeaderComponent = () => {
             router.push("/");
           }}
         >
-          <svg width="34" height="32" viewBox="0 0 39 37" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 2.64286H17.376C26.9725 2.64286 34.752 10.334 34.752 19.8214C34.752 29.3089 26.9725 37 17.376 37C7.77951 37 0 29.3089 0 19.8214V2.64286Z" fill="white" fill-opacity="0.7"></path><path d="M5.34647 0L5.34647 16.5179C5.34647 25.6404 12.8268 33.0357 22.0542 33.0357C31.2816 33.0357 38.7619 25.6404 38.7619 16.5179C38.7619 7.3953 31.2816 1.12078e-06 22.0542 7.22019e-07L5.34647 0Z" fill="white"></path></svg>
+          <svg
+            width="34"
+            height="32"
+            viewBox="0 0 39 37"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 2.64286H17.376C26.9725 2.64286 34.752 10.334 34.752 19.8214C34.752 29.3089 26.9725 37 17.376 37C7.77951 37 0 29.3089 0 19.8214V2.64286Z"
+              fill="white"
+              fill-opacity="0.7"
+            ></path>
+            <path
+              d="M5.34647 0L5.34647 16.5179C5.34647 25.6404 12.8268 33.0357 22.0542 33.0357C31.2816 33.0357 38.7619 25.6404 38.7619 16.5179C38.7619 7.3953 31.2816 1.12078e-06 22.0542 7.22019e-07L5.34647 0Z"
+              fill="white"
+            ></path>
+          </svg>
           <TitleIcon />
         </a>
         <div>
