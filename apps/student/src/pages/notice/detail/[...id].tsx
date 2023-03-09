cbbf7e5f66b5b98629938c8482711329a429789a
ldh7228 <ldh7228@gmail.com>
2023-03-09 09:46:53 +0900

    Error handling

--- apps/student/src/pages/notice/detail/[...id].tsx
+++ apps/student/src/pages/notice/detail/[...id].tsx
@@ -17,8 +17,6 @@ import Welfare from "../../../lib/components/student/Welfare";
 import HeaderComponent from "ui/components/StudentHeader";
 import { useRouter } from "next/router";
 import { Footer } from "ui/components/Footer";
-import Attachment from "../../../lib/components/student/attachment";
-import Portpoilo from "../../../lib/components/student/portpolio";
 
 const NoticeDetail = () => {
   const query = useRouter().query.id as string;
@@ -98,12 +96,6 @@ const NoticeDetail = () => {
             />
             <QualificationRequirements noticeInfo={NoticeInfo} />
             <Welfare noticeInfo={NoticeInfo} />
-            <Portpoilo noticeInfo={NoticeInfo} />
-            <Attachment
-              NoticeID={NoticeID}
-              NoticeInfo={NoticeInfo}
-              getStatus={getStatus}
-            />
           </DetailDiv>
         ) : (
           <></>
