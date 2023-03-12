04ba04a41b05c1413a1ed9f4330e6532c5231e80
ldh7228 <ldh7228@gmail.com>
2023-03-12 22:25:18 +0900

    Create Carousel

--- apps/student/src/pages/notice/detail/[...id].tsx
+++ apps/student/src/pages/notice/detail/[...id].tsx
@@ -1,12 +1,9 @@
 import {
-  applyNotice,
   getCompanyDetail,
   getCompanyDetailProps,
   getNoticeDetail,
   getNoticeDetailProps,
   getSupportStatus,
-  presigned,
-  reissue,
 } from "../../../axios/dist";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
@@ -25,15 +22,18 @@ const NoticeDetail = () => {
   const [NoticeInfo, setNoticeInfo] = useState<getNoticeDetailProps>();
   const [CompanyInfo, setCompanyInfo] = useState<getCompanyDetailProps>();
   const [NoticeID, setNoticeID] = useState<string[]>([]);
+  const [current, setCurrent] = useState<number>(0);
 
   useEffect(() => {
-    if (query)
+    if (query) {
       getNoticeDetail({ id: query }).then((res: getNoticeDetailProps) => {
         setNoticeInfo(res);
+        console.log(res);
         getCompanyDetail({ id: res.company.companyNumber }).then((res1) => {
           setCompanyInfo(res1);
         });
       });
+    }
   }, [query]);
 
   useEffect(() => {
@@ -52,7 +52,7 @@ const NoticeDetail = () => {
       <MainDiv>
         {NoticeInfo && CompanyInfo ? (
           <DetailDiv>
-            <img
+            {/* <img
               src={
                 CompanyInfo.companyIntroductionResponse.companyPhotoList[0]
                   ? CompanyInfo.companyIntroductionResponse.companyPhotoList[0]
@@ -60,7 +60,49 @@ const NoticeDetail = () => {
                   : ""
               }
               alt=""
-            />
+            /> */}
+            <Carousel>
+              <Arrow
+                scale={1}
+                onClick={() => {
+                  if (current === 0)
+                    setCurrent(
+                      CompanyInfo.companyIntroductionResponse.companyPhotoList
+                        .length - 1
+                    );
+                  else setCurrent(current - 1);
+                }}
+              >
+                <div />
+              </Arrow>
+              {CompanyInfo.companyIntroductionResponse.companyPhotoList.map(
+                (t) => (
+                  <span>
+                    <CarouselImg
+                      translateX={current * -1000}
+                      src={t.fileUrl}
+                      alt="company photo"
+                      placeholder="blur"
+                    />
+                  </span>
+                )
+              )}
+              <Arrow
+                scale={-1}
+                onClick={() => {
+                  if (
+                    current <
+                    CompanyInfo.companyIntroductionResponse.companyPhotoList
+                      .length -
+                      1
+                  )
+                    setCurrent(current + 1);
+                  else setCurrent(0);
+                }}
+              >
+                <div />
+              </Arrow>
+            </Carousel>
             <h1>
               {NoticeInfo.classificationResponse.map((t, i, a) => (
                 <>
@@ -98,10 +140,7 @@ const NoticeDetail = () => {
             <QualificationRequirements noticeInfo={NoticeInfo} />
             <Welfare noticeInfo={NoticeInfo} />
             <Portpoilo noticeInfo={NoticeInfo} />
-            <Attachment
-              NoticeID={NoticeID}
-              NoticeInfo={NoticeInfo}
-            />
+            <Attachment NoticeID={NoticeID} NoticeInfo={NoticeInfo} />
           </DetailDiv>
         ) : (
           <></>
@@ -113,6 +152,56 @@ const NoticeDetail = () => {
 };
 
 export default NoticeDetail;
+
+const Arrow = styled.div<{ scale: number }>`
+  position: absolute;
+  z-index: 2;
+  top: 50%;
+  cursor: pointer;
+  width: 80px;
+  height: 80px;
+  left: ${(props) => (props.scale === 1 ? 3 : 90)}%;
+  transform: scale(${(props) => props.scale}, 1);
+  background-color: rgba(255, 255, 255, 1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  box-shadow: 0 0 10px 3px rgba(0, 0, 0, 0.1);
+  div {
+    width: 30px;
+    height: 60px;
+    margin-top: 12px;
+    margin-right: 10px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-image: url("data:image/svg+xml,%3Csvg width='14' height='22' viewBox='0 0 14 22' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M13.8799 18.6126L5.86488 10.5801L13.8799 2.54758L11.4124 0.0800781L0.912383 10.5801L11.4124 21.0801L13.8799 18.6126Z' fill='%23242424'/%3E%3C/svg%3E%0A");
+    transition: all 0.2s ease;
+  }
+`;
+
+const Carousel = styled.div`
+  display: inline-flex;
+  white-space: nowrap;
+  width: 1000px;
+  height: 600px;
+  overflow-x: hidden;
+  position: relative;
+  text-align: center;
+  overflow-y: hidden;
+
+  span {
+    text-align: center;
+  }
+`;
+
+const CarouselImg = styled.img<{ translateX: number }>`
+  width: 1000px;
+  height: 600px;
+  object-fit: contain;
+  transition: 1s;
+  transform: translateX(${(props) => props.translateX}px);
+`;
 
 const MainDiv = styled.div`
   display: flex;
