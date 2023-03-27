378131860bebfae69e9a0beaf40ce3c3084bda3a
ldh7228 <ldh7228@gmail.com>
2023-03-27 21:13:08 +0900

    Bug Fix

--- apps/student/src/axios/main/auth/codeSend/index.ts
+++ apps/student/src/axios/main/auth/codeSend/index.ts
@@ -1,3 +1,4 @@
+import { Notice } from "@/src/lib/components/student/Alert";
 import request from "../../";
 
 export const codeSend = async ({ email }: { email: string }) => {
@@ -7,7 +8,16 @@ export const codeSend = async ({ email }: { email: string }) => {
     params: {
       email: email,
     },
-  }).catch((err) => {
-    console.log(err);
+  }).then(() => {
+    Notice({
+      message: "인증번호가 발송되었습니다.",
+      state: "success",
+    });
+  })
+  .catch(() => {
+    Notice({
+      message: "이미 있는 사용자입니다.",
+      state: "error",
+    });
   });
 };
