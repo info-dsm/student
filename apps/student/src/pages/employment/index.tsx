7a3bbe6bfa6f0bde326e6178d6bcaccca2f90811
ldh7228 <ldh7228@gmail.com>
2023-05-10 20:26:02 +0900

    Update EmployPage

--- apps/student/src/pages/employment/index.tsx
+++ apps/student/src/pages/employment/index.tsx
@@ -1,47 +1,66 @@
 import HeaderComponent from "@/../../packages/ui/components/StudentHeader";
+import { getEmploymentClass, getEmploymentClassProps } from "@/src/axios/dist";
+
 import ClassInfo from "@/src/lib/components/student/employmentPage/ClassInfo";
 import RoundGraph from "@/src/lib/components/student/employmentPage/RoundGraph";
+import Swiper from "@/src/lib/components/student/employmentPage/Swiper";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const EmploymentPage = () => {
-  const slide = () => {
-    return (
-      <>
-        {[].map((e) => (
-          <div></div>
-        ))}
-      </>
-    );
-  };
+  const classroom = [
+    "소프트웨어\n개발 1반",
+    "소프트웨어\n개발 2반",
+    "임베디드\n개발과",
+    "정보\n보안과",
+  ];
+  const [selectClass, setSelectClass] = useState(0);
+  const [classInfo, setClassInfo] = useState<getEmploymentClassProps>();
+
+  useEffect(() => {
+    getEmploymentClass({ classroom: selectClass }).then((res) => {
+      setClassInfo(res);
+    });
+  }, [selectClass]);
 
   return (
     <MainDiv>
       <HeaderComponent />
       <Nav>
-        <div>{`소프트웨어\n개발 1반`}</div>
-        <div>{`소프트웨어\n개발 2반`}</div>
-        <div>{`임베디드\n개발과`}</div>
-        <div>{`정보\n보안과`}</div>
-        <br />
+        <Dot transX={38 + selectClass * 140} />
+        {classroom.map((e, i) => (
+          <NavContent
+            onClick={() => {
+              setSelectClass(i);
+            }}
+            selected={i === selectClass}
+          >
+            <span>{e}</span>
+          </NavContent>
+        ))}
       </Nav>
       <Container>
-        <RoundGraph />
-        <ClassInfo />
+        {classInfo ? (
+          <>
+            <RoundGraph
+              rate={
+                ((classInfo.totalEmployedClassStudent /
+                  classInfo.totalClassStudent) *
+                100)
+              }
+            />
+            <ClassInfo classInfo={classInfo} />
+          </>
+        ) : (
+          <></>
+        )}
       </Container>
-      <Swiper></Swiper>
+      <Swiper />
     </MainDiv>
   );
 };
 
 export default EmploymentPage;
-
-const Swiper = styled.div`
-  width: 100vw;
-`;
-
-// const Slide = style.ddiv`
-    
-// `
 
 const MainDiv = styled.div`
   width: 100vw;
@@ -54,7 +73,7 @@ const MainDiv = styled.div`
 const Container = styled.div`
   position: relative;
   width: 100%;
-  margin-top: 70px;
+  margin-top: 50px;
   display: flex;
   align-items: center;
   justify-content: flex-end;
@@ -68,7 +87,32 @@ const Nav = styled.div`
   display: inline-flex;
   font-size: 18px;
   gap: 60px;
-  div {
-    text-align: center;
+  position: relative;
+`;
+
+const Dot = styled.div<{ transX: number }>`
+  position: absolute;
+  text-align: center;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: #593fff;
+  transform: translateX(${(props) => props.transX}px);
+  transition: 0.5s;
+  bottom: -20px;
+`;
+
+const NavContent = styled.div<{ selected: boolean }>`
+  text-align: center;
+  cursor: pointer;
+  font-weight: 700;
+  width: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+  span {
+    color: ${(props) => (props.selected ? "#593fff" : "#000")};
+    transition: 0.5s;
   }
 `;
