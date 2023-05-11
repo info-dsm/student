54644ea8c780959d6a9e9b15cc620f46d9afabf4
ldh7228 <ldh7228@gmail.com>
2023-05-11 13:21:51 +0900

    Update Design

--- apps/student/src/pages/employment/index.tsx
+++ apps/student/src/pages/employment/index.tsx
@@ -1,21 +1,14 @@
 import HeaderComponent from "@/../../packages/ui/components/StudentHeader";
+import { Spinner } from "@/../../packages/ui/dist";
 import { getEmploymentClass, getEmploymentClassProps } from "@/src/axios/dist";
-
+import BarGraph from "@/src/lib/components/student/employmentPage/BarGraph";
 import ClassInfo from "@/src/lib/components/student/employmentPage/ClassInfo";
-import RoundGraph from "@/src/lib/components/student/employmentPage/RoundGraph";
-import Swiper from "@/src/lib/components/student/employmentPage/Swiper";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const EmploymentPage = () => {
-  const classroom = [
-    "소프트웨어\n개발 1반",
-    "소프트웨어\n개발 2반",
-    "임베디드\n개발과",
-    "정보\n보안과",
-  ];
-  const [selectClass, setSelectClass] = useState(0);
   const [classInfo, setClassInfo] = useState<getEmploymentClassProps>();
+  const [selectClass, setSelectClass] = useState(0);
 
   useEffect(() => {
     getEmploymentClass({ classroom: selectClass }).then((res) => {
@@ -24,39 +17,25 @@ const EmploymentPage = () => {
   }, [selectClass]);
 
   return (
-    <MainDiv>
-      <HeaderComponent />
-      <Nav>
-        <Dot transX={1.97 + selectClass * 7.29} />
-        {classroom.map((e, i) => (
-          <NavContent
-            onClick={() => {
-              setSelectClass(i);
-            }}
-            selected={i === selectClass}
-          >
-            <span>{e}</span>
-          </NavContent>
-        ))}
-      </Nav>
+    <>
       {classInfo ? (
         <>
-          <Container>
-            <RoundGraph
-              rate={
-                (classInfo.totalEmployedClassStudent /
-                  classInfo.totalClassStudent) *
-                100
-              }
-            />
-            <ClassInfo classInfo={classInfo} />
-          </Container>
-          <Swiper classInfo={classInfo} />
+          <HeaderComponent />
+          <MainDiv>
+            <Container>
+              <BarGraph
+                selectClass={{ state: selectClass, setState: setSelectClass }}
+              />
+            </Container>
+            <Container>
+              <ClassInfo classInfo={classInfo} />
+            </Container>
+          </MainDiv>
         </>
       ) : (
-        <></>
+        <Spinner />
       )}
-    </MainDiv>
+    </>
   );
 };
 
@@ -65,6 +44,8 @@ export default EmploymentPage;
 const MainDiv = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  padding: 0 200px;
   * {
     font-family: "Pretendard Variable";
   }
@@ -72,47 +53,9 @@ const MainDiv = styled.div`
 
 const Container = styled.div`
   position: relative;
-  width: 100%;
-  margin-top: 50px;
+  width: 50%;
+  margin-top: 170px;
   display: flex;
   align-items: center;
-  justify-content: flex-end;
-  padding-right: 15.1vmax;
-  height: 50.53vmin;
-`;
-
-const Nav = styled.div`
-  padding: 15vmin 0 0 16.5vmax;
-  white-space: pre-line;
-  display: inline-flex;
-  font-size: 0.93vmax;
-  gap: 3.19vmax;
-  position: relative;
-`;
-
-const Dot = styled.div<{ transX: number }>`
-  position: absolute;
-  text-align: center;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  background-color: #593fff;
-  transform: translateX(${(props) => props.transX}vmax);
-  transition: 0.5s;
-  bottom: -20px;
-`;
-
-const NavContent = styled.div<{ selected: boolean }>`
-  text-align: center;
-  cursor: pointer;
-  font-weight: 700;
-  width: 4.16vmax;
-  display: flex;
   flex-direction: column;
-  align-items: center;
-  row-gap: 10px;
-  span {
-    color: ${(props) => (props.selected ? "#593fff" : "#000")};
-    transition: 0.5s;
-  }
 `;
