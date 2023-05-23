44264027df3ae8e3fd69b31a868b7fb8af1930f1
ldh7228 <ldh7228@gmail.com>
2023-05-23 22:51:59 +0900

    Create EmployGrid

---
+++ apps/student/src/lib/components/student/employmentPagev2/EmployGrid.tsx
@@ -0,0 +1,87 @@
+import { classArray } from "@/public/data";
+import { getEmploymentClass, getEmploymentClassProps } from "@/src/axios/dist";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const EmployGrid = () => {
+  const [employData, setEmployData] = useState<getEmploymentClassProps[]>();
+
+  useEffect(() => {
+    axios
+      .all(
+        [0, 1, 2, 3].map((e) => {
+          return getEmploymentClass({ classroom: e });
+        })
+      )
+      .then(
+        axios.spread((res1, res2, res3, res4) => {
+          setEmployData([res1, res2, res3, res4]);
+        })
+      );
+  }, []);
+
+  return (
+    <MainDiv>
+      {employData?.map((e1, i1) => (
+        <div>
+          <h1>
+            <span>{classArray[i1]}</span>
+            <span>
+              {e1.totalEmployedClassStudent}/{e1.totalClassStudent}명
+            </span>
+          </h1>
+          <ImageBox height={Math.ceil(e1.employmentList.length / 4) * 22.5}>
+            {e1.employmentList.map((e2, i2) => (
+              <img src={e2.company.companyLogo} alt="" />
+            ))}
+          </ImageBox>
+        </div>
+      ))}
+    </MainDiv>
+  );
+};
+export default EmployGrid;
+
+const ImageBox = styled.div<{ height: number }>`
+  margin-top: 0.7vmax;
+  display: grid;
+  height: ${(props) => props.height}%;
+  grid-template-columns: repeat(4, 1fr);
+  img {
+    width: 7.3vmax;
+    height: 6.4vmin;
+  }
+`;
+
+const MainDiv = styled.div`
+  width: 75%;
+  height: 100%;
+  padding: 30px;
+  display: grid;
+  padding-top: 5.5%;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 15px;
+  grid-row-gap: 20px;
+  > div {
+    background-color: #f8f8f9;
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    box-shadow: 0 0 15px 0 #10111240;
+    padding: 20px 28px;
+    > h1 {
+      margin: 0;
+      width: 100%;
+      display: inline-flex;
+      justify-content: space-between;
+      > span {
+        font-size: 1.3vmax;
+        + span {
+          font-size: 1vmax;
+          color: #1011128a;
+        }
+      }
+    }
+  }
+`;
