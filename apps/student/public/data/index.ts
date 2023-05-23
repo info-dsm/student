e2c4c4622de5d4830b10cc69a6a1f332061a3fe4
ldh7228 <ldh7228@gmail.com>
2023-05-23 22:51:43 +0900

    Add classArray

--- apps/student/public/data/index.ts
+++ apps/student/public/data/index.ts
@@ -2,3 +2,9 @@
 export {};
 export const frameRate = 1000 / 60;
 export const totalFrame = Math.round(1500 / frameRate);
+export const classArray = [
+  "소프트웨어 개발1반",
+  "소프트웨어 개발2반",
+  "임베디드 개발과",
+  "정보보안과",
+];
