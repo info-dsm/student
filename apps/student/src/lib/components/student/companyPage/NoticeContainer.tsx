bb0982afbbf83bc599d929df66890f3c70d89ae9
ldh7228 <ldh7228@gmail.com>
2023-06-22 14:22:00 +0900

    Create CompanyPageNotice

---
+++ apps/student/src/lib/components/student/companyPage/NoticeContainer.tsx
@@ -0,0 +1,37 @@
+import {
+  getCompanyDetailProps,
+  getWaitingNoticeListContentProps,
+} from "@/src/axios/dist";
+import styled from "styled-components";
+import CompanyPageNoticeBox from "./Notice";
+
+const CompanyPageNotice = ({
+  companyInfo,
+  noticeInfo,
+}: {
+  companyInfo: getCompanyDetailProps;
+  noticeInfo: getWaitingNoticeListContentProps[];
+}) => {
+  return (
+    <MainDiv>
+      <div>
+        <h2>채용중인 포지션</h2>
+        <CompanyPageNoticeBox info={noticeInfo}/>
+      </div>
+    </MainDiv>
+  );
+};
+export default CompanyPageNotice;
+
+const MainDiv = styled.div`
+  position: relative;
+  width: 100%;
+  margin-left: 40px;
+  > div {
+    top: 25px;
+    position: sticky;
+    >h2 {
+      font-size: 26px;
+    }
+  }
+`;
